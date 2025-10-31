81f338a5425f60c74e78279904319a734f0d7af8
Aetherx24 <iqbalfahrur@gmail.com>
2025-10-31 21:12:55 +0700

    chore: compose sections and footer in single-page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,20 @@ import About from "@/components/About";
 import Education from "@/components/Education";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className="bg-white font-sans text-black dark:bg-black dark:text-zinc-50">
       <Navbar />
-      <main className="pt-16">
+      <main>
         <Hero />
         <About />
         <Education />
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </div>
   );
 }
